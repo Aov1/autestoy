21186a0458cfced3806b9fa7a14e86eb52cd87fd
ThinkBook14P-aoiiix <jason001100@outlook.com>
2026-03-29 15:57:01 +0800

    。

--- tests/test_autestoy/conftest.py
+++ tests/test_autestoy/conftest.py
@@ -7,7 +7,7 @@ from autestoy.protocols.ssh import SSH, RemoteConfig
 def remote():
     return RemoteConfig(
         user="u0_a210",
-        ip="192.168.0.32",
+        ip="192.168.18.6",
         password="0402",
         port=8022,
     ).set_name("HUAWEI MATEPAD 12.2")

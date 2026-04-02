ece27f61f041705e33d864b27a98517b2fc82a36
ThinkBook14P-aoiiix <jason001100@outlook.com>
2026-04-03 00:04:08 +0800

    。

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
+from pprint import pprint
+
 import autestoy as at
+from autestoy.tools.result import Result
+
+res = Result(123)
+print(res.get())
+print(res.type())
+exit()
 
 conf = at.RemoteConfig(
-    user="aoiiix",
-    ip="127.0.0.1",
+    user="u0_a210",
+    ip="192.168.18.6",
     password="0402",
-    port=22,
+    port=8022,
 ).set_name("local")
-conn = at.SSH(conf)
-conn.exec_run('echo "Hello World"')
-ch = conn.create_channel(insert_cmd=None)
-# print(ch.prompt_now)
-ch.run("ls")
-ch.run("pwd")
+
+with at.SSH(conf) as conn:
+    conn.exec_run('echo "Hello World"')
+    ch = conn.create_channel(insert_cmd=None)
+    # print(ch.prompt_now)
+    ch.run("ls")
+    ch.run("pwd")
+
+    with conn.create_ftp() as ftp:
+        ftp.chdir("project/autestoy_sim/")
+        if "ftp_test" not in ftp.listdir():
+            ftp.mkdir("ftp_test/")
+        else:
+            at.Term.putsln("ftp_test already exists")
+        res = ftp.put("./main.py", "ftp_test/main.py")
+        ftp.listdir_attr()
+        # ftp.aty_channel.run("pwd")
+
+    ch.run("cd project/autestoy_sim/ftp_test/")
+    ch.run("ls")

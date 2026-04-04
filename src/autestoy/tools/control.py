91c9ce64f25d894a8a2548b9fa9448898007a421
ThinkBook14P-aoiiix <jason001100@outlook.com>
2026-04-04 21:43:43 +0800

    创建control工具，添加TrySeconds类

---
+++ src/autestoy/tools/control.py
@@ -0,0 +1,34 @@
+import time
+
+from ..export.term import Term
+from .ansi import AnsiColor, AnsiReset
+from .timestamp import Timestamp
+
+
+class TrySeconds:
+    """创建一个计时器，经过设置的时间后将返回False"""
+
+    def __init__(self, seconds: float):
+        self.seconds = seconds
+        self.start_timestamp = Timestamp()
+        self.info = f"TrySeconds({seconds:^8.2f}) from {Term.fmt_timestamp(self.start_timestamp)}:"
+        Term.putsln(
+            self.info + f"{AnsiReset} Start",
+            log_time=self.start_timestamp,
+            set_font_color=AnsiColor.yellow,
+        )
+        self.first_check = True
+
+    def __bool__(self) -> bool:
+        is_trying = time.time() - self.start_timestamp < self.seconds
+        if not is_trying:
+            res = "Timeout" if self.first_check else "Already Timeout"
+            self.first_check = False
+            Term.putsln(
+                self.info + f"{AnsiReset} {res}",
+                set_font_color=AnsiColor.yellow,
+            )
+        return is_trying
+
+    def check_timeout(self) -> bool:
+        return not bool(self)

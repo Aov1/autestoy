9eb98f743a3a51d5565d3747f0dcdac9e934b83f
ThinkBook14P-aoiiix <jason001100@outlook.com>
2026-04-15 12:30:18 +0800

    删除无用重载注释

--- src/autestoy/tools/datatype.py
+++ src/autestoy/tools/datatype.py
@@ -322,59 +322,6 @@ class Bits:
     def set_one_line_max_width(cls, one_line_max_width: int) -> None:
         cls._one_line_max_width = one_line_max_width
 
-    # @overload
-    # def __init__(self, value: bool, width: int) -> None:
-    #     """当value为bool时，width用于指定宽度，相当于将True值转换为1、False转换为0"""
-    #     ...
-
-    # @overload
-    # def __init__(self, value: bool) -> None:
-    #     """当value为bool且没有width参数时，相当于将True值转换为0b1、False转换为0b0，宽度都为1"""
-    #     ...
-
-    # @overload
-    # def __init__(self, value: int, width: int) -> None:
-    #     """
-    #     当value为int时必须指定宽度\n
-    #     eg: Bits(123, 8) , Bits(0x1234, 16)
-    #     """
-    #     ...
-
-    # @overload
-    # def __init__(self, value: str) -> None:
-    #     """
-    #     当value为str时，并且value可被解析成整数且带有位数标识，自动解析宽度\n
-    #     eg: Bits("16'h1234") , Bits("255_u8") , Bits("0b1000_1110_i32")
-    #     """
-    #     ...
-
-    # @overload
-    # def __init__(self, value: str, width: int) -> None:
-    #     """
-    #     当value为str，且value可被解析为不带宽度的int，width用于指定宽度\n
-    #     eg: Bits("123", 8) , Bits("0x1234", 16)\n
-    #     value可解析为带宽度的整形，且width给出了宽度时，width指定的宽度优先级大于字符串解析的宽度\n
-    #     eg: Bits("0x1234_u16", 32)-> 0x0000_1234 , Bits("0x1234_5678_i32", 16)-> 0x5678\n
-    #     值的注意的是，字符串解析包含了长度截断，width即使大于截断长度，数值依然被截断\n
-    #     eg: Bits("0x12345678_u16",32) -> Bits(0x0000_5678,32)\n
-    #     """
-    #     ...
-
-    # @overload
-    # def __init__(self, value: Bits, width: int | None = None) -> None:
-    #     """当value为Bits实例时，width用于指定宽度，相当于重置Bits的宽度"""
-    #     ...
-
-    # # @overload
-    # # def __init__(self, value: Bits) -> None:
-    # #     """当value为Bits实例时，width为None时相当于复制了Bits实例"""
-    # #     ...
-
-    # @overload
-    # def __init__(self, value: Iterable[tuple[int | str, int] | str | Bits]) -> None:
-    #     """当value为Iterable时，且Iterable的子类型是支持的初始化类型，创建拼接初始化"""
-    #     ...
-
     def __init__(
         self,
         value: BitsInitValue,
@@ -1041,5 +988,7 @@ class Register:
 
 
 class Packet:
-    def __init__(self, ) -> None:
-        pass
+    def __init__(
+        self,
+    ) -> None:
+        self.bits: Bits

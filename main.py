2f3902886682df70943fe6455624415a3219fe78
ThinkBook14P-aoiiix <jason001100@outlook.com>
2026-05-14 23:08:53 +0800

    .

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ from autestoy.export.messageio import (
 from autestoy.export.term import MessageTerminal
 from autestoy.tools.timestamp import Timestamp
 
+ip_now = "192.168.110.30"
+
+
 remote_conf = att.RemoteConfig(
     user="aoiiix",
     # host="192.168.51.12",
-    host="192.168.110.28",
+    host=ip_now,
     password="0402",
 ).set_name("vDebian13")
 
@@ -47,10 +50,17 @@ with (
     sftp.close()
 
 
-disp.join()
-
-
 #
 import subprocess as sp
 
 sp.run("pwd", shell=True)
+
+telnet_conf = att.TelnetConfig(
+    host=ip_now,
+    port=23,
+)
+tel = att.TelnetShell(telnet_conf, ("aoiiix", "0402"))
+
+tel.shell_run("ls")
+
+disp.join()
